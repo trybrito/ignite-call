0337f2f0b85664483677eb9e461a9f8f2f6f7556
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-02-26 13:51:21 -0300

    initial commit: project setup

---
+++ src/styles/globals.ts
@@ -0,0 +1,16 @@
+import { globalCss } from '@ignite-ui/react';
+
+export const globalStyles = globalCss({
+  '*': {
+    boxSizing: 'border-box',
+    margin: 0,
+    padding: 0,
+    '-webkit-font-smoothing': 'antialiased',
+    '-mos-osx-font-smoothing': 'grayscale',
+  },
+
+  body: {
+    backgroundColor: '$gray900',
+    color: '$gray100',
+  },
+});
