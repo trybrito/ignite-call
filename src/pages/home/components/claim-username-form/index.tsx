521fad6761e59d7e1b5a5becfa5cd36e62c29a87
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-03-02 20:16:34 -0300

    styles: homepage

---
+++ src/pages/home/components/claim-username-form/index.tsx
@@ -0,0 +1,56 @@
+import { Button, Text, TextInput } from '@ignite-ui/react'
+import { Form, FormAnnotation } from './styles'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(1, { message: 'O usuário precisa ter ao menos 1 letra' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens',
+    })
+    .transform((username) => username.toLowerCase()), // allows case insensitive letters and hifens
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
+export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  async function handleClaimUsername(formData: ClaimUsernameFormData) {
+    console.log(formData)
+  }
+
+  return (
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuário"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Digite o nome do usuário desejado'}
+        </Text>
+      </FormAnnotation>
+    </>
+  )
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,31 @@
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
+import Image from 'next/image'
+import previewHero from '../../assets/preview-hero.png'
+import { ClaimUsernameForm } from './components/claim-username-form'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+        <ClaimUsernameForm />
+      </Hero>
+      <Preview>
+        <Image
+          src={previewHero}
+          height={400}
+          quality={100}
+          alt="Calendário simbolizando a aplicação em funcionamento"
+          priority
+        />
+      </Preview>
+    </Container>
+  )
+}
