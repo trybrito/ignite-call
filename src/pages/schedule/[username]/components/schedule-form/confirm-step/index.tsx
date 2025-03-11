916b625b483aac968864ae2b652fc3fdc922448e
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-03-11 00:33:49 -0300

    styles: scheduling confirm page

---
+++ src/pages/schedule/[username]/components/schedule-form/confirm-step/index.tsx
@@ -0,0 +1,43 @@
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { CalendarBlank, Clock } from 'phosphor-react'
+
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+
+export function ConfirmStep() {
+  return (
+    <ConfirmForm as="form">
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Setembro de 2022
+        </Text>
+        <Text>
+          <Clock />
+          18:00
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput placeholder="Seu nome" />
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput type="email" placeholder="johndoe@example.com" />
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
+}
